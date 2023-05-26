cff536048646a2dd818c5f2482e78e0a6010b225
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-26 10:58:55 +0300

    getter and setter methods for person

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    # name setter 
+    def name=(name)
+        @name = name 
+    end
+    # job setter method
+    def job=(job)
+        @job = job
+    end
+
+    # job getter method
+    def job
+        @job
+    end
+
+
+    # name getter method
+    def name
+        @name
+    end
 
 end
